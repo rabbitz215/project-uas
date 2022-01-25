65c1f4803a4598d1e29ce62c6a9885ce03054c91
rabbitz215 <galangputra376@gmail.com>
2022-01-25 16:28:21 +0700

    UAS

---
+++ uas/assets/javascript/login.js
@@ -0,0 +1,22 @@
+const username = "admin";
+const password = "123";
+
+function ceklogin() {
+    let userlogin = document.getElementById("usernamelogin").value;
+    let passlogin = document.getElementById("passwordlogin").value;
+    if (userlogin === "" || userlogin === null) {
+        message("alert", "Username wajib diisi!!");
+
+    } else if (passlogin === "" || passlogin === null) {
+        message("alert", "Password wajib diisi!!");
+    } else {
+        if (userlogin === username && passlogin === password) {
+            alert("Login Berhasil, Selamat Datang " + userlogin);
+            window.location.href = "index.html";
+        } else {
+            alert("Username/Password Salah");
+            document.getElementById("usernamelogin").value = "";
+            document.getElementById("passwordlogin").value = "";
+        }
+    }
+}
